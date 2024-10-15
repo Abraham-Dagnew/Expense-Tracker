801d37d3277f43dd5c9cbb1d1cc5b9f50e52ebda
Abraham Dagnew <Ahaymero@gmail.com>
2024-10-14 23:45:17 -0400

    Login/Register

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.DB
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Expense_Tracker.DB;
 using Expense_Tracker.Models;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Expense_Tracker
@@ -14,6 +17,24 @@ namespace Expense_Tracker
             builder.Services.AddDbContext<ExpensesDB>(options =>
                 options.UseInMemoryDatabase("ExpensesDB")
             );
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+            builder.Services.AddIdentity<Users, IdentityRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+
 
 
 
@@ -39,6 +60,11 @@ namespace Expense_Tracker
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+        }
+
+        private static void AddEntityFrameworkstores<T>(WebApplicationBuilder builder)
+        {
+            throw new NotImplementedException();
         }
     }
 }
