065391ddbe0e909e1d8fc5f2a84a3d6acd9a83d3
Abraham Dagnew <Ahaymero@gmail.com>
2024-10-16 23:44:34 -0400

    Worked on Login and Register page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Expense_Tracker.Models;
+using Expense_Tracker.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register (RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+                var result = await
+            }
+            }
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
