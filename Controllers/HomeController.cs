0994eb979fd1993f4cda6e93eb99e88bb2a0075a
Abraham Dagnew <Ahaymero@gmail.com>
2024-11-02 23:11:30 -0400

    Final build for the expense tracker

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -75,10 +75,7 @@ namespace Expense_Tracker.Controllers
         {
             return View();
         }
-        public IActionResult Calendar()
-        {
-            return View();
-        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
